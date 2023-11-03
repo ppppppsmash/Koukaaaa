f4b49e26ae3650a6db3ea917d3b33a0aaea0ad72
ppppppsmash <pppppp.work@gmail.com>
2023-11-04 01:34:16 +0900

    chore: tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import "../app.css";
+</script>
+
+
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+
+<nav>
+	<a href="/about">メッセージ</a>
+	<a href="/business">主要事業</a>
+  <a href="/products">取扱商品</a>
+  <a href="/company">会社概要</a>
+  <a href="/organization">組織図</a>
+</nav>
+
+<slot></slot>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
